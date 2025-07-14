81d5614bbf13302da88d6f9382d95c030b0fbc0b
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-14 13:45:11 +0700

    first commit

---
+++ models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: false, // ✅ Password is optional (especially for OAuth)
+  },
+  profileImageUrl: String,
+  googleId: { type: String, unique: true, sparse: true },
+});
+
+export default mongoose.model("User", userSchema);
